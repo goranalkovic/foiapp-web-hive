b696da0a2565b78a00a750c8b4a1401fa15f55a4
Goran Alković <goranalkovic@users.noreply.github.com>
2020-01-24 06:10:19 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "/assets\LICENSE": "dfb3df3123ce64f6a27c02eb2cdd4e76",
 "/assets\packages\outline_material_icons\lib\outline_material_icons.ttf": "6b94994fffd9868330d830fcb18a6026",
 "/index.html": "f4dba618bb5da9faf9376b6a9cf9b74e",
-"/main.dart.js": "b1f1dadc78c77323f5f9bfbc10b4e964"
+"/main.dart.js": "0d8647ffebb1ea34ffbbcb3b458e840c"
 };
 
 self.addEventListener('activate', function (event) {
